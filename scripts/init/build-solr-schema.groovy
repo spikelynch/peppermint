8b71f7d1fd3517a5b35b5c2f2add7cdbd7224986
Shilo B <shiloworks@gmail.com>
2019-01-22 16:00:58 +1000

    Better init handling.

--- scripts/init/build-solr-schema.groovy
+++ scripts/init/build-solr-schema.groovy
@@ -44,6 +44,10 @@ def replaceExisting(existingFields, mainArr, addArr, replaceArr, forceReplace, m
     }
   }
   mainArr.each { entry ->
+    if (!existingFields) {
+      addArr << entry;
+      return
+    }
     def entryExist = existingFields.find matchClosure(entry)
     if (forceReplace) {
       if (entryExist) {
@@ -64,12 +68,18 @@ def replaceExisting(existingFields, mainArr, addArr, replaceArr, forceReplace, m
 
 def getSchema(core, schemaField) {
   def uri = "${config.solr.baseUrl}/solr/${core}/schema/${schemaField}?wt=json"
-  return configure {
-    request.uri = uri
-    request.contentType = "application/json"
-    // Unfortunately, SOLR returns Content-Type: "text/plain" for JSON responses, no amount of bargaining, i.e. '?wt=json' will convince it
-    response.parser "text/plain", NativeHandlers.Parsers.&json
-  }.get()
+  try {
+    return configure {
+      request.uri = uri
+      request.contentType = "application/json"
+      // Unfortunately, SOLR returns Content-Type: "text/plain" for JSON responses, no amount of bargaining, i.e. '?wt=json' will convince it
+      response.parser "text/plain", NativeHandlers.Parsers.&json
+    }.get()
+  } catch (e) {
+    logger.error("Failed schema retrieval")
+    logger.error(e)
+    return null;
+  }
 }
 
 def postSchema(core, commandStr) {
@@ -87,7 +97,9 @@ def processConfig(core, schemaField, responseFieldName, configArr, addCommandStr
   // replace all existing fields...
   def replaceFieldArr = []
   def addFieldArr = []
-  replaceExisting(getSchema(core, schemaField)[responseFieldName], configArr, addFieldArr, replaceFieldArr, forceReplace, matchClosureFn, skipExists)
+  def schema = getSchema(core, schemaField)
+  def existingFields = schema ? schema[responseFieldName] : null
+  replaceExisting(existingFields, configArr, addFieldArr, replaceFieldArr, forceReplace, matchClosureFn, skipExists)
   commandStrArr.addAll(generateCommandStr(replaceCommandStr, replaceFieldArr))
   commandStrArr.addAll(generateCommandStr(addCommandStr, addFieldArr))
 }
@@ -101,15 +113,15 @@ def deleteIndexData(core) {
   }.get()
 }
 
-def checkSolr() {
-  def uri = "${config.solr.baseUrl}/solr/admin/cores?action=STATUS"
+def checkSolr(coreName) {
+  def uri = "${config.solr.baseUrl}/solr/admin/cores?action=STATUS&core=${coreName}"
   return configure {
     request.uri = uri
     request.contentType = "application/json"
   }.get()
 }
 
-def waitForSolr() {
+def waitForSolr(coreName) {
   def solrUp = false;
   def maxTries = 5;
   def waitTime = 5000;
@@ -117,14 +129,19 @@ def waitForSolr() {
   while (!solrUp && tryCtr <= maxTries ) {
     try {
       tryCtr++
-      logger.info("Checking if solr is up...${tryCtr}");
-      logger.info(checkSolr())
-      logger.info("Solr is up!")
-      solrUp = true
+      logger.info("Checking if '${coreName}' solr is up...${tryCtr}");
+      def solrStat = checkSolr(coreName)
+      logger.info(solrStat)
+      if (solrStat.status[coreName].instanceDir) {
+        logger.info("Solr '${coreName}' is up!")
+        solrUp = true
+      } else {
+        throw new Exception("SOLR '${coreName}' still loading.")
+      }
     } catch (e) {
+      logger.info("SOLR '${coreName}' still down.. waiting.")
       logger.error(e)
-      logger.info("SOLR still down.. waiting.")
-      sleep(waitTime)
+      Thread.sleep(waitTime)
     }
   }
 }
@@ -135,11 +152,11 @@ def waitForSolr() {
 
 logger.info("SOLR schema builder starting...")
 if (config.solr.rebuildSchemaAlways) {
-  waitForSolr()
   logger.info("Rebuilding schema...deleting all existing data for each core.")
   def postDataStr = "{"
   config.solr.cores.each { core ->
     try {
+      waitForSolr(core)
       deleteIndexData(core)
     } catch(e) {
       logger.error(e)
@@ -149,6 +166,7 @@ if (config.solr.rebuildSchemaAlways) {
     def addField = config.solr.schema[core]['add-field']
     def addDynamicField = config.solr.schema[core]['add-dynamic-field']
     def addCopyField = config.solr.schema[core]['add-copy-field']
+
     if (addField && addField.size() > 0) {
       // Not replacing existing as fields might have dependent copyFields which will cause an error when replacing/deleting
       processConfig(core, 'fields', 'fields', addField, 'add-field', 'replace-field', commandStrArr, false, null, true)
